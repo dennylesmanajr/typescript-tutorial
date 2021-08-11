d06fe9367c2557ffedce6b490e72b807f8019fb8
Denny Lesmana <denny.lesmana@treessolutions.com>
2021-08-11 23:58:06 +0700

    learn ts protect type of data to prevent error in js file

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+// const character = 'luigi';
+// console.log('character: ', character);
+// const inputs = document.querySelectorAll('input');
+// console.log('inputs: ', inputs);
+// inputs.forEach(input => {
+//     console.log('input: ', input);
+// })
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log('circ: ', circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,18 @@
+// const character = 'luigi';
+// console.log('character: ', character);
+
+
+// const inputs = document.querySelectorAll('input');
+// console.log('inputs: ', inputs);
+
+// inputs.forEach(input => {
+//     console.log('input: ', input);
+
+// })
+
+const circ =  (diameter:number) => {
+    
+    return diameter * Math.PI;
+}
+
+console.log('circ: ', circ(7.5));
